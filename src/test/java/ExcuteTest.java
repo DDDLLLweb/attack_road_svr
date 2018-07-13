38765fbef93cfc78e95250de7f4b5a63d449e713
L1116 <976130720@qq.com>
2018-07-13 21:28:57 +0800

    aa

--- src/test/java/ExcuteTest.java
+++ src/test/java/ExcuteTest.java
@@ -1,12 +1,8 @@
-package baseclass;
-
-
 import com.el.jdbc.CustomJdbcConfig;
 import com.el.jdbc.MybatisConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
